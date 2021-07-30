aa6c24f3714f1852b8b6ccb3535186cb75f553cd
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-07-30 22:35:14 +0700

    Mini Test 1

---
+++ Mini Test 1/Q1.py
@@ -0,0 +1,6 @@
+num1=int(input("index of x 0-->6= "))#index of x 0-->6= 4
+num2=int(input("index of y 0-->6= "))#index of y 0-->6= 5
+x=[10,3,5.5,35,7,8,9]
+y=[0,2,5.5,8.6,19,20,3]
+mul=x[num1]*y[num2]
+print("multiply is = ",mul)
